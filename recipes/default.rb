2fc5011f217ccef78be9a52414457afd29b6736b
Luca Carrogu <carrogu@amazon.com>
2019-12-27 10:17:50 +0100

    Add support for RHEL
    Missing:
    * no DKMS support for Nvidia installation
    * no SGE support
    * no FSX Lustre support
    
    Signed-off-by: Luca Carrogu <carrogu@amazon.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,7 +15,7 @@
 # OR CONDITIONS OF ANY KIND, express or implied. See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe 'aws-parallelcluster::sge_install'
+# include_recipe 'aws-parallelcluster::sge_install'
 include_recipe 'aws-parallelcluster::torque_install'
 include_recipe 'aws-parallelcluster::slurm_install'
 
